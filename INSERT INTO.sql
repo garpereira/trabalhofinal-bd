1b4292d250ff7a73233df7128a31589e6c7fce53
garpereira <54422345+garpereira@users.noreply.github.com>
2022-02-02 17:22:11 -0300

    Add files via upload

---
+++ INSERT INTO.sql
@@ -0,0 +1,7 @@
+#Jogador(id_jogador, nome, apelido, idade, nro_vitorias, nro_derrotas)
+INSERT INTO Jogador
+	VALUES
+    (null, 'Yugi Muto', 'Yugi', 16, 5, 1),
+    (null, 'Seto Kaiba', 'Kaiba', 17, 4, 2),
+    (null, 'Ryo Bakura', 'Ryo', 16, 3, 6),
+    (null, 'Mai Valentine', 'Mai', 18, 4, 8);
