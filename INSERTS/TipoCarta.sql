16c7aa448ae9f516b58be5893f5a955b38448771
garpereira <54422345+garpereira@users.noreply.github.com>
2022-02-11 22:54:58 -0300

    Atualização arquivos

---
+++ INSERTS/TipoCarta.sql
@@ -0,0 +1,15 @@
+#um id para cada tipo de carta sejam elas (Normal, Continua, Campo, Rapida, Resposta, Fusao, Ritual, Efeito, Sincro, Xyz, Pendulo)
+#id_tipo_carta, tipo_carta
+INSERT INTO TiposCarta
+	VALUES
+    (null, 'Normal'),
+    (null, 'Continua'),
+    (null, 'Campo'),
+    (null, 'Rapida'),
+    (null, 'Resposta'),
+    (null, 'Fusao'),
+    (null, 'Ritual'),
+    (null, 'Efeito'),
+    (null, 'Sincro'),
+    (null, 'Xyz'),
+    (null, 'Pendulo');
