8ade320320437d4bf0d307f623aeaa6c45da140e
garpereira <54422345+garpereira@users.noreply.github.com>
2022-02-11 22:34:16 -0300

    atualizacao table creates

--- TABLE CREATES.sql
+++ TABLE CREATES.sql
@@ -12,87 +12,45 @@ CREATE TABLE Deck(id_deck integer not null auto_increment, nro_partidas integer,
 	FOREIGN KEY (id_jogador) REFERENCES Jogador(id_jogador)
 )Engine=innoDB;
 
-#Carta(id_carta, nome_carta, nro_edicao, descricao, #id_deck)
-CREATE TABLE Carta(id_carta integer, nome_carta varchar(40), nro_edicao integer, descricao varchar(500), id_deck integer,
-    PRIMARY KEY (id_carta),
-	FOREIGN KEY (id_deck) REFERENCES Deck(id_deck)
+#um id para cada tipo de carta sejam elas (Normal, Continua, Campo, Rapida, Resposta, Fusao, Ritual, Efeito, Sincro, Xyz, Pendulo)
+CREATE TABLE TiposCarta(id_tipo_carta integer not null auto_increment, tipo_carta varchar(20),
+	PRIMARY KEY (id_tipo_carta)
 )Engine=innoDB;
 
-<<<<<<< HEAD
+#Carta_Armadilha(#cod_serie,#id_carta , Normal, Resposta, Continua)
+#Carta_Magica(#cod_serie,#id_carta Normal, Continua, Campo, Rapida)
+#Carta_Monstro(#cod_serie,# id_carta, Normal, Fusao, Ritual, Efeito, Sincro, Xyz, Pendulo)
+CREATE TABLE NaturezaCarta(id_carta integer not null auto_increment, tipo_natureza varchar(15),
+	PRIMARY KEY (id_carta)
+)Engine=innoDB;
+
+#Carta(id_carta, nome_carta, nro_edicao, descricao, #id_deck, id_natureza, id_tipo_carta)
+CREATE TABLE Carta(id_carta integer not null auto_increment, nome_carta varchar(40), nro_edicao integer, descricao varchar(500), id_deck integer, id_natureza integer, id_tipo integer,
+    PRIMARY KEY (id_carta),
+	FOREIGN KEY (id_deck) REFERENCES Deck(id_deck),
+    FOREIGN KEY (id_natureza) REFERENCES NaturezaCarta(id_carta),
+    FOREIGN KEY (id_tipo) REFERENCES TiposCarta(id_tipo_carta)
+)Engine=innoDB;
+
 #Partida(cod_partida, tipo_campo, qtd_turnos, resultado)
 CREATE TABLE Partida(cod_partida integer not null auto_increment, tipo_campo varchar(10), qtd_turnos integer, vencedor integer, perdedor integer,
 	PRIMARY KEY (cod_partida),
     FOREIGN KEY (vencedor) REFERENCES Jogador(id_jogador),
     FOREIGN KEY (perdedor) REFERENCES Jogador(id_jogador)
-=======
-#Partida(cod_partida, tipo_campo, qtd_turnos, #resultado)
-CREATE TABLE Partida(cod_partida integer auto_increment, tipo_campo varchar(10), qtd_turnos integer, resultado integer not null,
-	PRIMARY KEY (cod_partida)
-    FOREIGN KEY (resultado) REFERENCES Resultado(cod_resultado)
->>>>>>> 8e9e039 (Adiciona tabela Resultado e modifica partida)
 )Engine=innoDB;
 
 #PartidaDeck(#id_deck, #cod_partida)
 CREATE TABLE PartidaDeck(id_deck integer, cod_partida integer,
 	PRIMARY KEY (id_deck),
-    PRIMARY KEY (cod_partida),
 	FOREIGN KEY (id_deck) REFERENCES Deck(id_deck),
     FOREIGN KEY (cod_partida) REFERENCES Partida(cod_partida)
 )Engine=innoDB;
 
-#JogadorPartida(#id_jogador, #cod_partida, data, hora)
-CREATE TABLE JogadorPartida(id_jogador integer, cod_partida integer, data date, hora time,
-	PRIMARY KEY (id_jogador),
-    PRIMARY KEY (cod_partida),
-    FOREIGN KEY (id_jogador) REFERENCES Jogador(id_jogador),
-    FOREIGN KEY (cod_partida) REFERENCES Partida(cod_partida)
-)Engine=innoDB;
-
-<<<<<<< HEAD
 #Turno(id_turno, id_jogador_1, id_jogador_2, hp_jogador_1, hp_jogador_2, #cod_partida)
 CREATE TABLE Turno(id_turno integer not null auto_increment, id_jogador_1 integer, id_jogador_2 integer, hp_jogador_1 integer, hp_jogador_2 integer, cod_partida integer,
-=======
-#Resultado(cod_resultado, #perdedor, #vencedor)
-CREATE TABLE Resultado(cod_resultado integer auto_increment, #perdedor varchar(20), #vencedor varchar(20),
-	PRIMARY KEY (cod_resultado),
-    FOREIGN KEY (perdedor) REFERENCES Jogador(nome),
-    FOREIGN KEY (vencedor) REFERENCES Jogador(nome)
-)Engine=innoDB;
-
-#Turno(id_turno, hp_jogador, qtde_cartas,#cod_partida)
-CREATE TABLE Turno(id_turno integer not null auto_increment, id_jogador_1 integer, id_jogador_2 integer, hp_jogador_1 integer, hp_jogador_2 integer, qtde_cartas integer, cod_partida integer,
->>>>>>> c419372 (Adiciona tabela Resultado e modifica partida)
 	PRIMARY KEY (id_turno),
     FOREIGN KEY (cod_partida) REFERENCES Partida(cod_partida),
     FOREIGN KEY (id_jogador_1) REFERENCES Jogador(id_jogador),
     FOREIGN KEY (id_jogador_2) REFERENCES Jogador(id_jogador)
 )Engine=innoDB;
 
-#um id para cada tipo de carta sejam elas (Normal, Continua, Campo, Rapida, Resposta, Fusao, Ritual, Efeito, Sincro, Xyz, Pendulo)
-CREATE TABLE TiposCarta(id_tipo_carta integer not null auto_increment, tipo_carta varchar(10),
-	PRIMARY KEY (id_tipo_carta)
-)Engine=innoDB;
-
-#Carta_Armadilha(#cod_serie,#id_carta , Normal, Resposta, Continua)
-CREATE TABLE CartaArmadilha(id_carta integer not null auto_increment, id_cartaDeck integer, id_tipo_carta integer,
-	PRIMARY KEY (id_carta),#foi adicionado este id para termos a nossa colecao de carta na tabela
-    PRIMARY KEY (id_cartaDeck),
-    FOREIGN KEY (id_cartaDeck) REFERENCES Carta(id_carta),
-    FOREIGN KEY (id_tipo_carta) REFERENCES TiposCarta(id_tipo_carta)
-)Engine=innoDB;
-	
-#Carta_Magica(#cod_serie,#id_carta Normal, Continua, Campo, Rapida)
-CREATE TABLE CartaMagica(id_carta integer not null auto_increment, id_cartaDeck integer, id_tipo_carta integer,
-	PRIMARY KEY (id_carta),#foi adicionado este id para termos a nossa colecao de carta na tabela
-    PRIMARY KEY (id_cartaDeck),
-    FOREIGN KEY (id_cartaDeck) REFERENCES Carta(id_carta),
-    FOREIGN KEY (id_tipo_carta) REFERENCES TiposCarta(id_tipo_carta)
-)Engine=innoDB;
-
-#Carta_Monstro(#cod_serie,# id_carta, Normal, Fusao, Ritual, Efeito, Sincro, Xyz, Pendulo)
-CREATE TABLE CartaMonstro(id_carta integer not null auto_increment, id_cartaDeck integer, id_tipo_carta integer,
-	PRIMARY KEY (id_carta),#foi adicionado este id para termos a nossa colecao de carta na tabela
-    PRIMARY KEY (id_cartaDeck),
-    FOREIGN KEY (id_cartaDeck) REFERENCES Carta(id_carta),
-    FOREIGN KEY (id_tipo_carta) REFERENCES TiposCarta(id_tipo_carta)
-)Engine=innoDB;
